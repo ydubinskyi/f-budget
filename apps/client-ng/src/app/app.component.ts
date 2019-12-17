50de3461ee266058f3b1ec9f1580f8967322615f
Yaroslav Dubinskyi <yaroslavd@google.com>
2019-12-17 17:06:35 +0100

    initial commit

---
+++ apps/client-ng/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'f-budget-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client-ng';
+}
